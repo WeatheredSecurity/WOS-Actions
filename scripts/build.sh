c76cdf90c3d97f72051cc5557cd17ad060151976
WeatheredCameron <cameron@weatheredsecurity.com>
2025-09-10 16:15:17 -0400

    force unmount to fix exit code 32

--- scripts/build.sh
+++ scripts/build.sh
@@ -56,7 +56,7 @@ function chroot_exit_teardown() {
     sudo chroot chroot umount /sys
     sudo chroot chroot umount /dev/pts
     sudo umount chroot/dev
-    sudo umount chroot/run
+    sudo umount -lf chroot/run
 }
 
 function check_host() {
