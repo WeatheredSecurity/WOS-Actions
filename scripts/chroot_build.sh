dd792ebb4fb770b10d7472af125cec9402f329e0
WeatheredCameron <cameron@weatheredsecurity.com>
2025-09-11 14:49:06 -0400

    fix iso not being produced

--- scripts/chroot_build.sh
+++ scripts/chroot_build.sh
@@ -155,6 +155,7 @@ function install_pkg() {
     # Kismet
     wget -O - https://www.kismetwireless.net/repos/kismet-release.gpg.key --quiet | gpg --dearmor | sudo tee /usr/share/keyrings/kismet-archive-keyring.gpg >/dev/null
     echo 'deb [signed-by=/usr/share/keyrings/kismet-archive-keyring.gpg] https://www.kismetwireless.net/repos/apt/release/jammy jammy main' | sudo tee /etc/apt/sources.list.d/kismet.list >/dev/null
+    apt update
     apt-get update
     apt install -y kismet
 
